91e85f6b4678cb61b1bfaec3a9754d6821f6cf69
Onur Kınay <43260714+onurkinay@users.noreply.github.com>
2024-11-30 18:47:38 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   swcMinify: true,
   images: {
     domains: ['images.unsplash.com', 'wembleypark.com'],
